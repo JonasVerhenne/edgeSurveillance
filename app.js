e2910de14e02528a716d1c1f948f6f568f795553
Jonas <jonas.verhenne@student.howest.be>
2021-01-12 12:25:50 +0100

    image from db + dropdown

--- app.js
+++ app.js
@@ -1,33 +1,25 @@
 let customHeaders = new Headers();
-customHeaders.append('Accept', 'application/json');
+//customHeaders.append('Accept', 'application/json');
+customHeaders.append('Content-Type', 'image/png');
 
 const fetchData = function(url)
 {
     return fetch(url, {headers: customHeaders})
-        .then(r => r.json()) // idem aan: function(r){return r.json()}
+        .then(r => r)
         .then(data => data);
 }
 
 const getImage = async function(name)
 {
-    try
-    {
-        console.log(`https://edgesurveillancefunction.azurewebsites.net/api/image/${name}`);
-        const data = await fetchData(`https://edgesurveillancefunction.azurewebsites.net/api/image/${name}`);
-        //console.log(data)
-        //
-        showImage(data);
-    }
-    catch(error)
-    {
-        console.error('An error occured', error);
-    }
+    img = new Image();
+    img = await fetchData(`https://edgesurveillancefunction.azurewebsites.net/api/image/${name}`);
+    showImage(img);
 }
 
 const showImage = function (image)
 {
-    $('#myImg').attr('src', data);
-    
+    let htmlQuestion = `<img src=${image.url}>`;
+    document.getElementById("myImg").innerHTML = htmlQuestion;
 }
 
 
@@ -38,22 +30,35 @@ const toggleSystem = function ()
     console.log(systemStatus);
 }
 
+const listenToDetection = function()
+{
+    console.log('click');
+    newDetectionValue = detection.value
+    console.log(newDetectionValue);
+    if (newDetectionValue != detectionValue)
+    {
+        console.log('new get')
+    }
+    detectionValue = newDetectionValue
+}
+
 const listenToUI = function ()
 {
+    toggle = document.querySelector(".js-switch");
+    detection = document.querySelector(".js-detection");
+
     toggle.addEventListener('change', toggleSystem);
+    detection.addEventListener('click', listenToDetection);
 }
 
 const init = function()
 {
     console.log("DOM Loaded");
-    toggle = document.querySelector(".js-switch");
     //get status from hub
     systemStatus = true;
+    detectionValue = "all"
     listenToUI();
-    getImage('test.png')
-    const image = document.createElement('img');
-    image.src  = '/test.png';
-    document.getElementById('myImg2').appendChild(image)
+    //getImage('test2.png');
 }
 
 document.addEventListener('DOMContentLoaded', init);
